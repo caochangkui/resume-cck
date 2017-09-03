64169aa56a83f6884eb1bd5b7d2db02cdaad0fa7
wangyan <cckwy@163.com>
2017-09-03 18:46:02 +0800

    我的简历

---
+++ js/addLoadEvent.js
@@ -0,0 +1,30 @@
+﻿/*---------------------当前时间----------------------*/
+function two_char(n) {
+	return n >= 10 ? n : "0" + n;
+}
+function time_fun() {
+	var mydate = new Date();
+	var sec=mydate.getSeconds();     //setSeconds()	设置 Date 对象中的秒钟 (0 ~ 59)。
+	setInterval(function () {
+		sec++;
+		var date = new Date();      //Date 对象会自动把当前日期和时间保存为其初始值     
+		date.setSeconds(sec);
+		var h = date.getHours(), m = date.getMinutes(), s = date.getSeconds();
+		document.getElementById("mytime").innerText = date.getFullYear()+"年" +(date.getMonth()+1)+"月" +date.getDate()+"日" + " " +two_char(h) + ":" + two_char(m) + ":" + two_char(s);
+	}, 1000);       //js返回的月份是从0开始算起的，0表示1月，11表示12月
+}
+
+/*---------------------添加多个onload事件----------------------*/
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
+addLoadEvent(time_fun); 
---
+++ js/func.js
@@ -0,0 +1,24 @@
+﻿//鼠标滑过立方体显示内容 
+$(document).ready(function(){
+//	旋转立方体上的字显示与隐藏
+	$(".map-show li ").hover(function(){
+		$(this).find("span").stop(true,true).fadeIn(600);
+	},function(){
+		$(this).find("span").stop(true,true).fadeOut(600);	
+	});
+	
+//点击按钮效果
+	$(".button input").click(function(){
+		$(this).addClass("current").siblings().removeClass("current"); /*给1,2,3,4,5,6点击时添加样式*/
+	});
+	
+	//返回顶部
+//	$("#goTopBtn").click(function(){  
+//		$(this).css("transform","translateY(200px)").css("transition","1s");
+//		$('body').animate({scrollTop:0},500);
+//	})
+}); 
+
+
+
+//$("p").css("background-color","yellow");animate({scrollTop:0},500);
